6ba19bc9887991ffd3dd371f9cdbc88193375985
Mini-Cynthia <89430952+Mini-Cynthia@users.noreply.github.com>
2021-10-18 16:37:00 +0800

    Add files via upload
    initialize

---
+++ src/main/java/com/mini/cynhr/DugoHrApplication.java
@@ -0,0 +1,13 @@
+package com.mini.cynhr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DugoHrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DugoHrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mini/cynhr/DugoHrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mini.cynhr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DugoHrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
